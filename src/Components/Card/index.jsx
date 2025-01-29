220738c41de7591312bd138b3bc0af7e386da889
Sebastian <sebastian.sanhueza.t@usach.cl>
2025-01-29 13:32:14 -0300

    Se muestran los productos desde la Api de platzi, y se modifica el componente Card para recibir datos como props y se implementa la carga de productos en la página de inicio

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,13 +1,13 @@
-const Card = () => {
+const Card = (data) => {
 	return (
 		<div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
 			<figure className="relative mb-2 w-full h-4/5">
 				<span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3  py-2">
-					Electronics
+					{data.data.category.name}
 				</span>
 				<img
-					src="https://images.pexels.com/photos/1649771/pexels-photo-1649771.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-					alt="headphones"
+					src={data.data.images[0]}
+					alt={data.data.title}
                     className="size-full object-cover rounded-lg"
 				/>
 				<div className="absolute top-0 right-0 flex justify-center items-center bg-white size-6 rounded-full m-2 px-2 py-1">
@@ -15,8 +15,8 @@ const Card = () => {
 				</div>
 			</figure>
 			<p className="flex justify-between">
-				<span className="text-sm font-light">Headphones</span>
-				<span className="text-sm font-medium">$300</span>
+				<span className="text-sm font-light">{data.data.title}</span>
+				<span className="text-sm font-medium">${data.data.price}</span>
 			</p>
 		</div>
 	);
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,14 +1,30 @@
-import Card from "../../Components/Card"
-import Layout from "../../Components/Layout"
+import { useState, useEffect } from "react";
+import Card from "../../Components/Card";
+import Layout from "../../Components/Layout";
 // import './App.css'
 
 function Home() {
-    return (
-        <Layout>
-            Hola Mundo bebé, soy el Home
-            <Card />
-        </Layout>
-    )
+	const [items, setItems] = useState(null);
+
+	useEffect(() => {
+		fetch("https://api.escuelajs.co/api/v1/products")
+			.then((response) => response.json())
+			.then((data) => {
+				setItems(data);
+				console.log(data);
+			});
+	}, []);
+
+	return (
+		<Layout>
+			Hola Mundo bebé, soy el Home
+			<div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+				{items?.map((item) => (
+					<Card key={item.id} data={item} />
+				))}
+			</div>
+		</Layout>
+	);
 }
 
-export default Home
+export default Home;
