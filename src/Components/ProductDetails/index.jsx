a1be866d94334e0bf9e67eb342eb99514793a93e
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-02-06 13:45:32 -0300

    Abrir y cerrar el aside

--- src/Components/ProductDetails/index.jsx
+++ src/Components/ProductDetails/index.jsx
@@ -1,12 +1,19 @@
 import { XMarkIcon } from '@heroicons/react/24/solid'
+import { useContext } from 'react';
+import { ShopppingCartContext } from "../../Context";
 import './styles.css';
 
 const ProductDetails = () => {
+    const context = useContext(ShopppingCartContext);
+
     return (
-        <aside className='Product-details flex flex-col fixed right-0 border border-black rounded bg-white p-5'>
+        <aside className={`${context.isProductDetailOpen ? 'flex' : 'hidden' } Product-details flex-col fixed right-0 border border-black rounded bg-white p-5`}>
             <div className='flex justify-between items-center'>
                 <h2 className='font-medium text-xl'>Details</h2>
-                <div>
+                <div 
+                    className='cursor-pointer'
+                    onClick={context.closeProductDetail}
+                >
                     <XMarkIcon className='h-5 w-5' />
                 </div>
             </div>
--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,12 +5,26 @@ export const ShopppingCartContext = createContext();
 // Creamos un contexto y lo tenemos que proveer a los demás y eso se hace con un provider que contenga (wrapper) a los demás componentes
 
 export const ShoppingCartProvider = ({ children }) => {
+    // Shopping Cart - Increment quantity
     const [count, setCount] = useState(0);
+
+    // Product Details - Open / close
+    const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+    const openProductDetail = () => setIsProductDetailOpen(true);
+    const closeProductDetail = () => setIsProductDetailOpen(false);
+
+    // Product Details - Show product details
+    const [productToShow, setProductToShow] = useState({});
 
     return (
         <ShopppingCartContext.Provider value={{
             count,
-            setCount
+            setCount,
+            openProductDetail,
+            closeProductDetail,
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             { children }    
         </ShopppingCartContext.Provider>
