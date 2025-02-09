8b5587dc038891152dc4c4a091b6f1f83785beb9
Sebastián Sanhueza Tapia <sebastian.sanhueza.t@usach.cl>
2025-02-09 17:58:33 -0300

    se añade el total de la orden en el CheckoutSideMenu y se mejora la estructura del componente

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -1,42 +1,56 @@
-import { useContext } from 'react';
+import { useContext } from "react";
 import { ShopppingCartContext } from "../../Context";
-import OrderCard from '../OrderCard';
-import { XMarkIcon } from '@heroicons/react/24/solid'
-import './styles.css';
+import OrderCard from "../OrderCard";
+import { XMarkIcon } from "@heroicons/react/24/solid";
+import "./styles.css";
 
 const CheckoutSideMenu = () => {
-    const context = useContext(ShopppingCartContext);
-    
-    console.log('cart: ', context.cartProducts);
+	const context = useContext(ShopppingCartContext);
 
-    return (
-        <aside className={`${context.isChechoutSideMenuOpen ? 'flex' : 'hidden' } checkout-side-menu top-20 flex-col gap-2 fixed right-0 border border-black rounded bg-white p-5`}>
-            <div className='flex justify-between items-center'>
-                <h2 className='font-medium text-xl'>My Order</h2>
-                <div 
-                    className='cursor-pointer'
-                    onClick={context.closeChechoutSideMenu}
-                >
-                    <XMarkIcon className='h-5 w-5' />
-                </div>
-            </div>
-            <div className='flex flex-col gap-2 scrollable-cards'>
-            {
-                context.cartProducts.map((product, index) => {
-                    return (
-                        <OrderCard 
-                            key={`${product.id}-${index}-${Math.random().toFixed(5)}`}
-                            title={product.title}
-                            imageUrl={product.images}
-                            price={product.price}
-                            quantity={product.quantity}
-                        />
-                    )
-                })
-            }
-            </div>
-        </aside>
-    );
+	console.log("cart: ", context.cartProducts);
+
+	return (
+		<aside
+			className={`${
+				context.isChechoutSideMenuOpen ? "flex" : "hidden"
+			} checkout-side-menu top-20 flex-col gap-2 fixed right-0 border border-black rounded bg-white p-5`}
+		>
+			<div className="flex justify-between items-center">
+				<h2 className="font-medium text-xl">My Order</h2>
+				<div className="flex justify-between gap-2 items-center">
+					<p className="font-medium text-xl">
+						Total: $
+						{
+                        context.cartProducts.length > 0 ?
+                        context.cartProducts.reduce((acc, product) => {
+							return acc + product.price * product.quantity;
+						}, 0) 
+                        : 0
+                        }
+					</p>
+					<div
+						className="cursor-pointer"
+						onClick={context.closeChechoutSideMenu}
+					>
+						<XMarkIcon className="h-5 w-5" />
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col gap-2 scrollable-cards">
+				{context.cartProducts.map((product, index) => {
+					return (
+						<OrderCard
+							key={`${product.id}-${index}-${Math.random().toFixed(5)}`}
+							title={product.title}
+							imageUrl={product.images}
+							price={product.price}
+							quantity={product.quantity}
+						/>
+					);
+				})}
+			</div>
+		</aside>
+	);
 };
 
 export default CheckoutSideMenu;
